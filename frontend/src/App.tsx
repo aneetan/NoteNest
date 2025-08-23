fd9de7303ef5b9f84f239252ad794b5018b7d988
aneetan <anitaneupane2061@gmail.com>
2025-08-23 22:34:36 +0545

    feat: completed  the login/register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<div> Hello from NoteNest </div>} />
+          <Route path='/' element={<Login/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Register/>} />
         </Routes>
       </BrowserRouter>
     </>
