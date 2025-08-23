66735326d8ba3ee17fbd19de1ad159b900a228b5
aneetan <anitaneupane2061@gmail.com>
2025-08-23 22:17:25 +0545

    init: database configuration completed

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import { connectToDatabase } from './config/dbconfig';
+
+require('dotenv').config();
+
+const app = express();
+const PORT = 3000;
+
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+connectToDatabase()
+    .then(() => {
+        //routes
+
+        app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))  
+    })
+    .catch((e) => {
+        console.log("Failed to initialize application", e);
+    })
+
+ 
